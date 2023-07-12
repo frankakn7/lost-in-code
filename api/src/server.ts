fc100ed1c041f4c60c69ee199cf580bbd52fd8ae
Maxim Strzebkowski <m.strzebkowski@stud.uni-goettingen.de>
2023-07-12 17:08:57 +0200

    Added php server with sanatization and error handling

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,54 @@
+import { Socket } from "dgram";
 import express, { Request, Response } from "express";
+import { IncomingMessage, ServerResponse } from "http";
+import httpProxy from "http-proxy";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+const proxy = httpProxy.createProxyServer();
+
 app.get("/api", (req: Request, res: Response) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
+app.post("/api/php", (req: Request, res: Response) => {
+    proxy.web(req, res, {
+        target: 'http://php_executor:6000', // Replace with the appropriate URL of your other server
+    });
+});
+
+// Handle errors that occur during the proxy request
+proxy.on("error", (err:Error, req:IncomingMessage, res) => {
+  console.error(err);
+  (res as ServerResponse<IncomingMessage>).writeHead(500);
+  res.end("There was an error with the PHP code");
+});
+
+// proxy.on('proxyRes', function (proxyRes, req:IncomingMessage, res:ServerResponse) {
+//   var body = '';
+//   proxyRes.on('data', function (data: Buffer) {
+//       body += data;
+//   });
+//   proxyRes.on('end', function () {
+//       let responseBody = JSON.parse(body);
+
+//       // If there's an error in the response, handle it
+//       if (responseBody.error) {
+//           console.error(responseBody.error);
+//           console.log(responseBody)
+//           res.statusCode = 500;
+//           // res.end(JSON.stringify({error: 'There was an error with the PHP code'}));
+//           res.end(body);
+//           return;
+//       }
+
+//       // If no error, just forward the response
+//       res.end(JSON.stringify(responseBody));
+//   });
+// });
+
+
 app.listen(port, () => {
-  console.log("Server running on port: "+port);
+    console.log("Server running on port: " + port);
 });
---
+++ php-server/server.php
@@ -0,0 +1,43 @@
+<?php
+// Start the output buffer
+ob_start();
+
+// Custom Error Handler
+function customErrorHandler($errno, $errstr, $errfile, $errline) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+
+// Register the custom error handler
+set_error_handler("customErrorHandler");
+
+// Register a function to be executed upon script shutdown
+register_shutdown_function(function() {
+    $error = error_get_last();
+    if ($error !== NULL) {
+        // Handle fatal error
+        ob_end_clean(); // Clean the output buffer
+        echo json_encode(array('error' => $error['message']));
+    }
+});
+
+try {
+    // Read the PHP code from the POST request
+    $code = $_POST['code'];
+
+    // Use eval() to execute the PHP code
+    eval($code);
+
+    // Get the output from the buffer and clean it
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    // Send the output back as the response
+    echo json_encode(array('result' => $output));
+} catch (Exception $e) {
+    // Clean the output buffer
+    ob_end_clean();
+
+    // Return error as response
+    echo json_encode(array('error' => $e->getMessage()));
+}
+?>
