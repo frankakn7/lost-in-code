0cd63270218d277575a7440d5d3d341e657f8b50
Haris Kurpejovic <haris.kurpejovic@stud.uni-goettingen.de>
2023-07-16 02:19:36 +0200

    Create the ProgressBar class (although empty)

---
+++ Game/src/ui/progress.ts
@@ -0,0 +1,13 @@
+import * as Phaser from "phaser";
+
+export default class ProgressBar extends Phaser.Scene {
+    constructor() {
+        super("Progress Bar");
+    }
+
+    create() {}
+
+    public reset() {}
+
+    public doOneProgress() {}
+}
