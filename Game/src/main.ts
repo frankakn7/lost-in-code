5e870bf3fd451752aae09326901d099399c7bacb
Haris Kurpejovic <haris.kurpejovic@stud.uni-goettingen.de>
2023-07-22 18:39:10 +0200

    Probably implemented proper scaling

--- Game/src/main.ts
+++ Game/src/main.ts
@@ -6,12 +6,20 @@ import './font.css';
 import LoginView from "./views/loginView";
 import { application } from "express";
 
+
 /**
  * Testing the tile config
  */
 
+const SIZE_WIDTH_SCREEN = 375*2.5;
+const SIZE_HEIGHT_SCREEN = 812*2.5;
+const MAX_SIZE_WIDTH_SCREEN = 750*2;
+const MAX_SIZE_HEIGHT_SCREEN= 1624*2;
+const MIN_SIZE_WIDTH_SCREEN = 375*2;
+const MIN_SIZE_HEIGHT_SCREEN = 812*2;
+
 const gameConfig: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     width: 375,
     height: 812,
     pixelArt: true,
@@ -25,16 +33,29 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     input: {
         activePointers: 3
     },
+
     scale: {
         // Fit to window
         // width: window.innerWidth,
         // height: window.innerHeight,
-        mode: Phaser.Scale.RESIZE,
+        mode: Phaser.Scale.FIT,
+        parent: 'game',
+        width: SIZE_WIDTH_SCREEN,
+        height: SIZE_HEIGHT_SCREEN,
+        min: {
+            width: MIN_SIZE_WIDTH_SCREEN,
+            height: MIN_SIZE_HEIGHT_SCREEN
+        },
+        max: {
+            width: MAX_SIZE_WIDTH_SCREEN,
+            height: MAX_SIZE_HEIGHT_SCREEN
+        }
         // Center vertically and horizontally
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        autoRound: true
+        // autoCenter: Phaser.Scale.CENTER_BOTH,
+        // autoRound: true
 
     },
+
     scene: new RootNode(),
     // scene: new LoginView(),
     // scene: new ChatView(),
