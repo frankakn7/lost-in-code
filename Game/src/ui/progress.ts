46dd6237f59a1a135dbeaa831a6155ae904c3293
Haris Kurpejovic <haris.kurpejovic@stud.uni-goettingen.de>
2023-07-24 15:12:28 +0200

    Comment progress bar

--- Game/src/ui/progress.ts
+++ Game/src/ui/progress.ts
@@ -2,16 +2,25 @@ import * as Phaser from "phaser";
 import RootNode from "../views/rootNode";
 import {globalEventBus} from "../helpers/globalEventBus";
 
+/**
+ * ProgressBar displays the progress of the player in the current room.
+ */
 export default class ProgressBar extends Phaser.Scene {
-    private _bar;
-    private _rootNode;
+    private _bar; // The progress bar graphics object.
+    private _rootNode; // The root node of the game or application.
 
+    /**
+     * Creates an instance of ProgressBar.
+     * @param rootNode
+     */
     constructor(rootNode: RootNode) {
         super("Progress Bar");
         this._rootNode = rootNode;
     }
 
-    //
+    /**
+     * Creates the progress bar.
+     */
     create() {
         this._bar = this.add.graphics();
         this.reset();
@@ -23,6 +32,9 @@ export default class ProgressBar extends Phaser.Scene {
         }).bind(this));
     }
 
+    /**
+     * Updates the progress bar.
+     */
     public updateBar() {
         const progress = this._rootNode.getCurrentRoom().getFinishedTaskObjectsCount() / this._rootNode.getCurrentRoom().getTaskObjectCount();
         console.log(progress)
@@ -31,6 +43,9 @@ export default class ProgressBar extends Phaser.Scene {
         this._bar.fillRect(55, 55, progress * totalWidthInner, 40);
     }
 
+    /**
+     * Resets the progress bar.
+     */
     public reset() {
         const totalWidth = this.cameras.main.displayWidth - 100;
         this._bar.clear();
