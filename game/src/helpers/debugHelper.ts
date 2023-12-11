aea5e933a41dc793138172cd41266efc38fcdd0e
Maxim Strzebkowski <m.strzebkowski@stud.uni-goettingen.de>
2023-12-11 15:12:09 +0100

    disabled debug messages

--- game/src/helpers/debugHelper.ts
+++ game/src/helpers/debugHelper.ts
@@ -1,14 +1,13 @@
-class DebugHelper{
-
-    private _debugging = true;
+class DebugHelper {
+    private _debugging = false;
 
     /**
      * Console logs the identifyingMessage in all caps with prefaced ### and the value seperately
      * @param identifyingMessage - message to be printed before the value
      * @param value - value to be printed
      */
-    public logValue(identifyingMessage: string, value: any){
-        if(this._debugging){
+    public logValue(identifyingMessage: string, value: any) {
+        if (this._debugging) {
             this.logString(identifyingMessage);
             console.log(value);
         }
@@ -18,10 +17,10 @@ class DebugHelper{
      * logs the given text all caps and prefaced ###
      * @param txt
      */
-    public logString(txt: string){
-        if(this._debugging){
-            const transformedString = "### "+txt.toUpperCase()
-            console.log(transformedString)
+    public logString(txt: string) {
+        if (this._debugging) {
+            const transformedString = "### " + txt.toUpperCase();
+            console.log(transformedString);
         }
     }
 }
